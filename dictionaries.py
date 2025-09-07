44d3dc67390727d1c8619aabb5bba3bc52953eca
lowlowCodee <laurenceg079@gmail.com>
2025-09-07 20:18:02 +0800

    pushed

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+
+
+student = {
+    "name": "Laurence",
+    "age": 20,
+    "course": "BSIT"
+}
+
+if "course" in student:
+    print("Yes! 'course' is one of the keys")
---
+++ list.py
@@ -0,0 +1,10 @@
+
+
+names = ["Laurence", "JB", "Victoria", "Kiesha", "John Doe", "Michael", "Nimo", "Juan"]
+            #-0       -1       -2         -3         -4         -5        -6      -7
+            #-8       -7       -6         -5         -4         -3        -2      -1
+            
+if "JB" in names:
+    print("Yes! 'JB' is in the list")
+else:
+    print("Not in the list")
